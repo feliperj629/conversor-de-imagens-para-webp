10f001f286c98e9649360572782cfced85d9281f
Felipe Alves de Oliveira <feliperj629@gmail.com>
2022-10-13 09:14:35 -0300

    Carregando função

---
+++ convert_jpg_webp.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from PIL import Image
+
+
+def convert_to_webp(source):
+    """Convert image to webp.
+
+    Args:
+        source (pathlib.Path): Path to source image
+
+    Returns:
+        pathlib.Path: path to new image
+    """
+    destination = source.with_suffix(".webp")
+
+    image = Image.open(source)  # Open image
+    image.save(destination, format="webp")  # Convert image to webp
+
+    return destination
+
+
+if __name__ == '__main__':
+    paths = Path("jpg").glob("**/*.*")
+    for path in paths:
+        webp_path = convert_to_webp(path)
+        print(webp_path)
+
